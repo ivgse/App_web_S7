db27d51bebb3b82ca96ca7980f8bbac58f97a00c
GISE <iivaans@hotmail.com>
2025-03-17 12:55:15 -0600

    Se configuro la app. Se creo el botón y la casilla

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # App_web_S7
+
 Desarrollo de aplicación web para mostrar data de anuncios de autos. Proyecto de Sprint 7 Tripleten.
+
+Se utiliza un archivo sobre anuncios de carros en EEUU. La aplicación utiliza el df para crear graficos con información de los autos: modelo, año, kilometraje, etc...
+
+Mediante botones y casillas de verificación, mostramos la información elegida.
---
+++ app.py
@@ -0,0 +1,22 @@
+"""Aplicación para mostrar información de anuncios de autos en EEUU"""
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+autos = pd.read_csv('/Users/IvanSoto/Desktop/Python/Sprint7_Git/App/App_web_S7/vehicles_us.csv')
+
+st.header('Información de los anuncios de autos en EEUU')
+
+boton_hist=st.button('Crear histograma')
+
+if boton_hist:
+    st.write('Histograma de precios x distancia')
+    fig = px.histogram(autos, x='odometer', y='price', color='type')
+    st.plotly_chart(fig, use_container_width=True)
+
+opcion_scatter=st.checkbox('Crear Dispersión', value=True)
+
+if opcion_scatter:
+    st.write('Dispersión de precios x año')
+    fig = px.scatter(autos, x='model_year', y='price', color='type')
+    st.plotly_chart(fig, use_container_width=True)
